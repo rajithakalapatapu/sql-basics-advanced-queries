941ef74899d2449f2a5f3e81c7eea97bbda7d999
rajitha <rajithakalapatapu9@gmail.com>
2018-09-28 08:03:53 -0500

    insert values into db from python - skip errors

--- code/import_records.py
+++ code/import_records.py
@@ -1,13 +1,23 @@
 files = ['DEPARTMENT', 'EMPLOYEE', 'DEPT_LOCATIONS','PROJECT','WORKS_ON','DEPENDENT']
 
+import cx_Oracle
+connection = cx_Oracle.connect('system/123456@localhost:1521/ORCLCDB')
+cursor = connection.cursor()
+
+def insert_into_db(table, values):
+    try:
+        sql = 'insert into {} values ({})'.format(table, values.rstrip())
+        print(sql)
+        cursor.execute(sql)
+        connection.commit()
+    except cx_Oracle.IntegrityError as ie:
+        connection.rollback()
+    except cx_Oracle.DatabaseError as de:
+        connection.rollback()
+
 for file in files:
     print('Processing file {} starts'.format(file))
     with open('{}.txt'.format(file), 'r') as file_to_read:
-        print('Read the file {}'.format(file)) 
-        with open('{}_queries.sql'.format(file), 'w+') as file_to_write:
-            print('Writing')
-            file_to_write.write("SET ECHO ON;\n")
-            for line in file_to_read.readlines():
-                file_to_write.write("insert into {} values({}); \n".format(file,line))
-            print('Done Writing queries from {} file'.format(file))
-            file_to_write.write("SET ECHO OFF;\n")
+        print('Read the file {}'.format(file))
+        for line in file_to_read.readlines():
+            insert_into_db(file, line);
