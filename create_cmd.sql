c467f847a6eb727b006e87b6349d5d19cb1df761
rajitha <rajithakalapatapu9@gmail.com>
2018-09-28 01:09:14 -0500

    modify data field sizes

--- create_cmd.sql
+++ create_cmd.sql
@@ -7,28 +7,28 @@ drop table Dependent;
 drop table Department CASCADE CONSTRAINTS;
 drop table Employee CASCADE CONSTRAINTS;
 
-Create table Employee(Fname varchar(15) NOT NULL, 
-		      Minit varchar(15) NOT NULL,
-		      Lname varchar(15) NOT NULL,
+Create table Employee(Fname varchar(50) NOT NULL, 
+		      Minit varchar(50) NOT NULL,
+		      Lname varchar(50) NOT NULL,
 		      Ssn int NOT NULL,
 		      Bdate date NOT NULL,
-		      Address varchar(15) NOT NULL,
+		      Address varchar(256) NOT NULL,
 		      Sex char(10) NOT NULL,
 		      Salary int NOT NULL,
 		      Super_ssn int NOT NULL,
 		      Dno int NOT NULL);
 
-Create table Department(Dname varchar(15) NOT NULL,
+Create table Department(Dname varchar(32) NOT NULL,
 			Dnumber int NOT NULL,
 			Mgr_ssn int NOT NULL,
 			Mgr_start_date date NOT NULL);
 
 Create table Dept_Locations(Dnumber int NOT NULL,
-		            Dlocation varchar(15) NOT NULL);
+		            Dlocation varchar(32) NOT NULL);
 
-Create table Project(Pname varchar(20) NOT NULL,
+Create table Project(Pname varchar(32) NOT NULL,
 		     Pnumber int NOT NULL,
-		     Plocation varchar(20) NOT NULL,
+		     Plocation varchar(32) NOT NULL,
 		     Dnum int NOT NULL);
 
 Create table Works_on(Essn int NOT NULL,
