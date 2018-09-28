0793c822be7b1348a89146300d1695851f7abcdf
rajitha <rajithakalapatapu9@gmail.com>
2018-09-28 00:57:33 -0500

    generate queries for all tables

--- code/import_records.py
+++ code/import_records.py
@@ -1,4 +1,13 @@
-with open('DEPARTMENT.txt', 'r') as file_to_read: 
-    with open('department_queries.sql', 'w+') as file_to_write:
-        for line in file_to_read.readlines():
-            file_to_write.write("insert into DEPARTMENT values({}); \n".format(line[:-1]))
+files = ['DEPARTMENT', 'EMPLOYEE', 'DEPT_LOCATIONS','PROJECT','WORKS_ON','DEPENDENT']
+
+for file in files:
+    print('Processing file {} starts'.format(file))
+    with open('{}.txt'.format(file), 'r') as file_to_read:
+        print('Read the file {}'.format(file)) 
+        with open('{}_queries.sql'.format(file), 'w+') as file_to_write:
+            print('Writing')
+            file_to_write.write("SET ECHO ON;\n")
+            for line in file_to_read.readlines():
+                file_to_write.write("insert into {} values({}); \n".format(file,line))
+            print('Done Writing queries from {} file'.format(file))
+            file_to_write.write("SET ECHO OFF;\n")
