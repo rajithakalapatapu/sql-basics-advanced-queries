9be8173a5971ee813a5624e12983629dd0f904f4
rajitha <rajithakalapatapu9@gmail.com>
2018-09-27 21:29:53 -0500

    capture output of create cmds

--- create_cmd.sql
+++ create_cmd.sql
@@ -1,34 +1,61 @@
+drop table Dept_Locations;
+drop table Works_on;
+drop table Project;
+drop table Dependent;
 drop table Employee;
 drop table Department;
-drop table Dept_Locations;
-drop table Project;
-drop table Works_on;
-drop table Dependent;
 
-Create table Employee(Fname varchar(15),Minit varchar(15),Lname varchar(15),Ssn int,Bdate date,Address varchar(15),Sex char(10),Salary int,Super_ssn int, Dno int);
+Create table Employee(Fname varchar(15) NOT NULL, 
+		      Minit varchar(15) NOT NULL,
+		      Lname varchar(15) NOT NULL,
+		      Ssn int NOT NULL,
+		      Bdate date NOT NULL,
+		      Address varchar(15) NOT NULL,
+		      Sex char(10) NOT NULL,
+		      Salary int NOT NULL,
+		      Super_ssn int NOT NULL,
+		      Dno int NOT NULL);
 
-Create table Department(Dname varchar(15),Dnumber int,Mgr_ssn int,Mgr_start_date date);
+Create table Department(Dname varchar(15) NOT NULL,
+			Dnumber int NOT NULL,
+			Mgr_ssn int NOT NULL,
+			Mgr_start_date date NOT NULL);
 
-Create table Dept_Locations(Dnumber int,Dlocation varchar(15));
+Create table Dept_Locations(Dnumber int NOT NULL,
+		            Dlocation varchar(15) NOT NULL);
 
-Create table Project(Pname varchar(20),Pnumber int,Plocation varchar(20),Dnum int));
+Create table Project(Pname varchar(20) NOT NULL,
+		     Pnumber int NOT NULL,
+		     Plocation varchar(20) NOT NULL,
+		     Dnum int NOT NULL);
 
-Create table Works_on(Essn int,Pno int,Hours);
+Create table Works_on(Essn int NOT NULL,
+	              Pno int NOT NULL,
+		      Hours int NOT NULL);
 
-Create table Dependent(Essn int,Dependent_name varchar(20),Sex char(15),Bdate date,Relationship varchar(15));
+Create table Dependent(Essn int NOT NULL, 
+		       Dependent_name varchar(20) NOT NULL,
+                       Sex char(15) NOT NULL,
+	               Bdate date NOT NULL,
+	               Relationship varchar(15) NOT NULL);
 
-
-Alter table Employee add(primary key(Ssn),foreign key Dno references Department(Dnumber));
 
 Alter table Department add(primary key(Dnumber));
 
-Alter table Dept_Locations add(primary key(Dnumber,Dlocation),foreign key Dnumber references Department(Dnumber));
+Alter table Employee add(primary key(Ssn),foreign key (Dno) references Department(Dnumber));
 
-Alter table Project add(primary key(Pnumber),foreign key Dnum references Department(Dnumber));
 
-Alter table Works_on add(primary key(Essn,Pno),foreign key Essn references Employee(Ssn),foreign key Pno references Project(Pnumber));
 
-Alter table Dependent add(primary key(Dependent_name),foreign key Essn references Employee(Ssn));
+
+Alter table Dept_Locations add(primary key(Dnumber,Dlocation),foreign key (Dnumber) references Department(Dnumber));
+
+Alter table Project add(primary key(Pnumber),foreign key (Dnum) references Department(Dnumber));
+
+Alter table Works_on add(primary key(Essn,Pno),foreign key (Essn) references Employee(Ssn),foreign key (Pno) references Project(Pnumber));
+
+Alter table Dependent add(primary key(Dependent_name),foreign key (Essn) references Employee(Ssn));
+
+
 
 
 
