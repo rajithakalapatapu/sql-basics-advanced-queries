7994c1d6747af9f440b58b28330c05d5b956409b
rajitha <rajithakalapatapu9@gmail.com>
2018-10-01 22:12:37 -0500

    fix styling errors

--- code/import_records.py
+++ code/import_records.py
@@ -1,8 +1,9 @@
-files = ['DEPARTMENT', 'EMPLOYEE', 'DEPT_LOCATIONS','PROJECT','WORKS_ON','DEPENDENT']
+files = ['DEPARTMENT', 'EMPLOYEE', 'DEPT_LOCATIONS', 'PROJECT', 'WORKS_ON', 'DEPENDENT']
 
 import cx_Oracle
 connection = cx_Oracle.connect('system/123456@localhost:1521/ORCLCDB')
 cursor = connection.cursor()
+
 
 def insert_into_db(table, values):
     try:
@@ -15,9 +16,10 @@ def insert_into_db(table, values):
     except cx_Oracle.DatabaseError as de:
         connection.rollback()
 
+
 for file in files:
     print('Processing file {} starts'.format(file))
     with open('{}.txt'.format(file), 'r') as file_to_read:
         print('Read the file {}'.format(file))
         for line in file_to_read.readlines():
-            insert_into_db(file, line);
+            insert_into_db(file, line)
